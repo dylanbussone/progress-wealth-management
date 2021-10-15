3a58367a81fe7263d5d6e997aed5266dd885f539
Dylan Bussone <dylanb@zillow.com>
2021-10-14 19:50:57 -0700

    normalize icons a bit, fix mob header

--- components/header.js
+++ components/header.js
@@ -19,7 +19,7 @@ const HEADER_LINKS = [
     {
         text: 'Schedule Now',
         href: '/schedule',
-    }
+    },
 ];
 
 const Header = () => {
@@ -29,25 +29,53 @@ const Header = () => {
     };
 
     return (
-        <header className={`${styles.header} ${mobileMenuActive && styles.mobile_menu_active}`}>
-            <div className={styles.header_content}>
-                <Link href="/" passHref>
-                    <button className={styles.logo_button} />
-                </Link>
-                <button className={styles.mobile_toggle} onClick={toggleMobileMenu}>
-                    <Icon name={mobileMenuActive ? 'close' : 'menu'} size={32} color="icon_white" />
-                </button>
-                <ul className={styles.link_list}>
-                    {HEADER_LINKS.map(link => (
-                        <li key={link.text}>
-                            <Link href={link.href || ''} passHref>
-                                {link.text}
-                            </Link>
-                        </li>
-                    ))}
-                </ul>
-            </div>
-        </header>
+        <React.Fragment>
+            {/* Desktop header */}
+            <header className={styles.header}>
+                <div className={styles.header_content}>
+                    <Link href="/" passHref>
+                        <button className={styles.logo_button} />
+                    </Link>
+                    <ul className={styles.link_list}>
+                        {HEADER_LINKS.map(link => (
+                            <li key={link.text}>
+                                <Link href={link.href}>
+                                    {link.text}
+                                </Link>
+                            </li>
+                        ))}
+                    </ul>
+                </div>
+            </header>
+            {/* Mobile header */}
+            <header className={styles.mobile_header}>
+                <div className={styles.mobile_header_content}>
+                    <Link href="/" passHref>
+                        <button className={styles.mobile_logo_button} />
+                    </Link>
+                    <button className={styles.mobile_toggle} onClick={toggleMobileMenu}>
+                        <Icon
+                            name={mobileMenuActive ? 'close' : 'menu'}
+                            size={mobileMenuActive ? 16 : 22}
+                            color="icon_white"
+                        />
+                    </button>
+                </div>
+                {mobileMenuActive && (
+                    <ul className={styles.mobile_link_list}>
+                        {HEADER_LINKS.map(link => (
+                            <li key={link.text}>
+                                <Link
+                                    href={link.href}
+                                    onClick={() => setMobileMenuActive(false)}>
+                                    {link.text}
+                                </Link>
+                            </li>
+                        ))}
+                    </ul>
+                )}
+            </header>
+        </React.Fragment>
     );
 };
 
