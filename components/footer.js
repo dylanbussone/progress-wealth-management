e3c9e943c6afdefcb3ecf1986d4a9228f6bf5751
Dylan Bussone <dylanb@zillow.com>
2021-11-05 12:57:49 -0700

    Footer logo / color

--- components/footer.js
+++ components/footer.js
@@ -5,19 +5,24 @@ import styles from '../styles/footer.module.css';
 const Footer = () => (
     <footer className={styles.footer}>
         <div className={styles.footer_content}>
-            <div className={styles.quote}>
-                <h5>
-                    Planning is bringing the future into the present so that you can do something
-                    about it now.
-                </h5>
-                <span>-- Alan Lakein</span>
+            <div>
+                <div className={styles.quote}>
+                    <h5>
+                        Planning is bringing the future into the present so that you can do
+                        something about it now.
+                    </h5>
+                    <span>-- Alan Lakein</span>
+                </div>
+                <ul>
+                    <li>
+                        <Link href="/legal">Legal</Link>
+                    </li>
+                    <li>&copy; 2021 Progress Wealth Management</li>
+                </ul>
             </div>
-            <ul>
-                <li>
-                    <Link href="/legal">Legal</Link>
-                </li>
-                <li>&copy; 2021 Progress Wealth Management</li>
-            </ul>
+            <div className={styles.logo}>
+                <img src="/logo.svg" alt="Progress Wealth Management" />
+            </div>
         </div>
     </footer>
 );
