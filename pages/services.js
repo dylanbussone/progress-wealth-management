9d42644fd2eeca317066fa9e54107a1a51c2dcf5
Dylan Bussone <dylanb@zillow.com>
2021-11-18 10:51:07 -1000

    Styling fixups, add bolded text to services

--- pages/services.js
+++ pages/services.js
@@ -42,31 +42,42 @@ const Services = () => {
                 <h2>Common Questions We Help Our Clients Answer</h2>
 
                 <ul className={styles.common_questions}>
-                    <li>How can I plan for the future in the most tax efficient way? </li>
+                    <li>
+                        How can I plan for the future in the most{' '}
+                        <span className={styles.bold}>tax efficient</span> way?{' '}
+                    </li>
                     <li>
                         How should I approach my stock options to ensure I’m not wasting any money?
                     </li>
                     <li>
-                        What can be done today to ensure I’m not throwing away any of my employer
-                        sponsored benefits?
+                        What can be done today to ensure I’m not throwing away any of my{' '}
+                        <span className={styles.bold}>employer sponsored benefits</span>?
                     </li>
                     <li>
-                        How can I ensure that I’m truly maximizing my earning potential early on and
-                        in doing so, getting paid what I’m worth?
+                        How can I ensure that I’m truly{' '}
+                        <span className={styles.bold}>maximizing my earning potential</span> early
+                        on and in doing so, getting paid what I’m worth?
                     </li>
                     <li>What can I do today to decrease my tax burden? </li>
                     <li>
-                        How do I protect myself from liability of getting sued either by clients or
-                        from whatever could happen?
+                        How do I <span className={styles.bold}>protect myself</span> from liability
+                        of getting sued either by clients or from whatever could happen?
                     </li>
-                    <li>What can I do differently to grow my money more effectively? </li>
-                    <li>How should I approach paying off my student loans?</li>
+                    <li>
+                        What can I do differently to{' '}
+                        <span className={styles.bold}>grow my money</span> more effectively?{' '}
+                    </li>
+                    <li>
+                        How should I approach{' '}
+                        <span className={styles.bold}>paying off my student loans</span>?
+                    </li>
                     <li>How can I grow my rental property empire in a smart way? </li>
                     <li>Do I need an estate plan and if so, how should it look?</li>
                 </ul>
             </Section>
             <Section>
                 <h2>Still have questions?</h2>
+                <br />
                 <DropdownQuestions />
             </Section>
         </Main>
