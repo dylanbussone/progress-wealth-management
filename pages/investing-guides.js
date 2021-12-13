f0f746dfd079caffda9442f70604443a310c3490
Dylan Bussone <dylanbussone@gmail.com>
2021-12-12 17:06:45 -0800

    add new pages

---
+++ pages/investing-guides.js
@@ -0,0 +1,17 @@
+import React from 'react';
+import Main from '../components/main';
+import Section from '../components/section';
+import styles from '../styles/pricing.module.css';
+
+const InvestingGuides = () => {
+    return (
+        <Main>
+            <Section>
+                <h1>Investing Guides</h1>
+                <div>stuff</div>
+            </Section>
+        </Main>
+    );
+};
+
+export default InvestingGuides;
---
+++ pages/knowledge-center.js
@@ -0,0 +1,17 @@
+import React from 'react';
+import Main from '../components/main';
+import Section from '../components/section';
+import styles from '../styles/pricing.module.css';
+
+const KnowledgeCenter = () => {
+    return (
+        <Main>
+            <Section>
+                <h1>Knowledge Center</h1>
+                <div>stuff</div>
+            </Section>
+        </Main>
+    );
+};
+
+export default KnowledgeCenter;
