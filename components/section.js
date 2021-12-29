ea3be3a86551a696a2f557b4c608f342d2e27f96
Dylan Bussone <dylanbussone@gmail.com>
2021-12-29 13:39:13 -0800

    Allow for section themes

--- components/section.js
+++ components/section.js
@@ -1,12 +1,18 @@
 import React from 'react';
 import styles from '../styles/section.module.css';
 
-const Section = ({ children, ...args }) => {
+const Section = ({ theme, children, ...args }) => {
     const props = {
         ...args,
-        className: `${styles.section} ${args.className ? args.className : ''}`,
+        className: `${styles.section} ${theme ? styles[theme] : ''} ${
+            args.className ? args.className : ''
+        }`,
     };
-    return <section {...props}>{children}</section>;
+    return (
+        <section {...props}>
+            <div className={styles.section_content}>{children}</div>
+        </section>
+    );
 };
 
 export default Section;
