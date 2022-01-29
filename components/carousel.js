30a54593ece1e5d597e20dc1d7c7242d402b0da9
Dylan Bussone <dylanbussone@gmail.com>
2022-01-29 11:28:56 -0800

    Use normal img for carousel to prevent unmounting weirdness with next/image

--- components/carousel.js
+++ components/carousel.js
@@ -1,63 +1,53 @@
 import React, { useEffect } from 'react';
 import Glide from '@glidejs/glide';
-import Image from '../components/image';
 import { Link } from './link';
 import styles from '../styles/carousel.module.css';
-import Businessinsider from '../public/carousel/businessinsider.png';
-import Gobankinrates from '../public/carousel/gobankingrates.png';
-import Insure from '../public/carousel/insure.png';
-import Msn from '../public/carousel/msn.png';
-import Nasdaq from '../public/carousel/nasdaq.png';
-import Nerdwallet from '../public/carousel/nerdwallet.jpg';
-import Sioux from '../public/carousel/sioux.png';
-import Thinkadvisor from '../public/carousel/thinkadvisor.png';
-import Yahoo from '../public/carousel/yahoo.png';
 
 const CAROUSEL_ITEMS = [
     {
         desc: 'NerdWallet',
         link: 'https://www.nerdwallet.com/article/finance/credit-score/nerdwallet-millennial-money-build-credit-now',
-        img: Nerdwallet,
+        img: '/carousel/nerdwallet.jpg',
     },
     {
         desc: 'Sioux City Journal',
         link: 'https://siouxcityjournal.com/business/article_36911812-4327-5c9a-b318-a68ad25d84e0.html',
-        img: Sioux,
+        img: '/carousel/sioux.png',
     },
     {
         desc: 'Insure.com',
         link: 'https://www.insure.com/home-insurance/how-much-dwelling-coverage-do-i-need/',
-        img: Insure,
+        img: '/carousel/insure.png',
     },
     {
         desc: 'Business Insider',
         link: 'https://markets.businessinsider.com/news/stocks/millennial-money--now-is-the-time-to-build-your-credit-10865264',
-        img: Businessinsider,
+        img: '/carousel/businessinsider.png',
     },
     {
         desc: 'ThinkAdvisor',
         link: 'https://www.thinkadvisor.com/2021/12/23/what-8-advisors-will-do-differently-in-2022-advisors-advice/',
-        img: Thinkadvisor,
+        img: '/carousel/thinkadvisor.png',
     },
     {
         desc: 'Nasdaq',
         link: 'https://www.nasdaq.com/articles/dol-planning-a-big-change-to-the-fiduciary-rule-2021-08-06',
-        img: Nasdaq,
+        img: '/carousel/nasdaq.png',
     },
     {
         desc: 'MSN Money',
         link: 'https://www.msn.com/en-us/money/news/how-to-prepare-for-what-social-security-wont-cover-in-retirement/ar-AAPY9ur',
-        img: Msn,
+        img: '/carousel/msn.png',
     },
     {
         desc: 'Yahoo Finance',
         link: 'https://finance.yahoo.com/news/millennials-share-biggest-money-regrets-110016585.html',
-        img: Yahoo,
+        img: '/carousel/yahoo.png',
     },
     {
         desc: 'GoBankingrates.com',
         link: 'https://www.gobankingrates.com/money/financial-planning/signs-should-fire-financial-planner/',
-        img: Gobankinrates,
+        img: '/carousel/gobankingrates.png',
     },
 ];
 
@@ -82,7 +72,7 @@ const Carousel = () => {
                             <li key={item.link} className="glide__slide">
                                 <div className={styles.carousel_item}>
                                     <Link href={item.link}>
-                                        <Image src={item.img} alt={item.desc} />
+                                        <img src={item.img} alt={item.desc} />
                                     </Link>
                                 </div>
                             </li>
