4cbc5b3fb9f5ec63b7c8be62514c26a57ba9b741
Dylan Bussone <dylanbussone@gmail.com>
2022-03-13 15:25:10 -0700

    secrets lol

--- firebase.ts
+++ firebase.ts
@@ -2,13 +2,13 @@ import { initializeApp } from 'firebase/app';
 // import { getFirestore } from 'firebase/firestore/lite';
 
 const firebaseConfig = {
-    apiKey: process.env.NEXT_PUBLIC_FIREBASE_API_KEY,
-    authDomain: process.env.NEXT_PUBLIC_FIREBASE_AUTH_DOMAIN,
-    projectId: process.env.NEXT_PUBLIC_FIREBASE_PROJECT_ID,
-    storageBucket: process.env.NEXT_PUBLIC_FIREBASE_STORAGE_BUCKET,
-    messagingSenderId: process.env.NEXT_PUBLIC_FIREBASE_MESSAGING_SENDER_ID,
-    appId: process.env.NEXT_PUBLIC_FIREBASE_APP_ID,
-    measurementId: process.env.NEXT_PUBLIC_FIREBASE_MEASUREMENT_ID,
+    apiKey: 'AIzaSyAmttynSJ2Ubht6N9pWefiPuXGK8W0r4F0',
+    authDomain: 'progress-wealth-management.firebaseapp.com',
+    projectId: 'progress-wealth-management',
+    storageBucket: 'progress-wealth-management.appspot.com',
+    messagingSenderId: '717934113207',
+    appId: '1:717934113207:web:958b07412b8cd6f271f832',
+    measurementId: 'G-NFTPYHQYL8',
 };
 
 const app = initializeApp(firebaseConfig);
